16a944452a7d0fa130156bab731c60bd618826b2
Yan Naing <yannaing.dev@gmail.com>
2026-03-26 21:18:40 +0630

    Incrrasing access token life

--- core/settings.py
+++ core/settings.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes = 60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours = 24),
     'REFRESH_TOKEN_LIFETIME': timedelta(days = 7),
     'ROTATE_REFRESH_TOKENS': True,
 }
